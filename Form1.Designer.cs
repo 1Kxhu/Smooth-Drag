24b2450c9e264b43df264fb25f9e1ce11b0be5d5
BuildTools <unconfigured@null.spigotmc.org>
2023-07-10 01:09:16 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace smooth_drag_opensource
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(12, 12);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(325, 33);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(244)))), ((int)(((byte)(251)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace smooth_drag_opensource
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            smoothMoveTimer = new System.Windows.Forms.Timer();
+            smoothMoveTimer.Interval = 1; // Adjust this value if needed
+            smoothMoveTimer.Tick += SmoothMoveTimer_Tick;
+            smoothness = trackBar1.Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isDragging = false;
+        private Point mouseOffset;
+        public static float smoothness = 4f; // Adjust this value to control the smoothness of dragging
+
+        private System.Windows.Forms.Timer smoothMoveTimer;
+
+        // when the mouse moves
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point mousePos = PointToScreen(e.Location);
+                mousePos.Offset(-mouseOffset.X, -mouseOffset.Y);
+
+                // calculate the distance to move in the x and y directions
+                float targetX = mousePos.X;
+                float targetY = mousePos.Y;
+
+                float dx = targetX - Location.X;
+                float dy = targetY - Location.Y;
+
+                // start the smooth move timer and set the increment values
+                smoothMoveTimer.Start();
+                smoothMoveTimer.Tag = new PointF(dx / smoothness, dy / smoothness);
+            }
+        }
+
+        // when the mouse button is released
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            // stop dragging and the smooth move timer
+            isDragging = false;
+            smoothMoveTimer.Stop();
+        }
+
+        // when the mouse button is pressed down
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // start dragging and record the mouse offset
+            isDragging = true;
+            mouseOffset = new Point(e.X, e.Y);
+        }
+
+        // when the smooth move timer ticks
+        private void SmoothMoveTimer_Tick(object sender, EventArgs e)
+        {
+            // get the increment values from the timer's tag
+            PointF increment = (PointF)smoothMoveTimer.Tag;
+
+            // update the form's location based on the increment values
+            float newX = Location.X + increment.X;
+            float newY = Location.Y + increment.Y;
+            Location = new Point((int)newX, (int)newY);
+
+            // if the increment values are very small, stop the timer
+            if (Math.Abs(increment.X) < 1 && Math.Abs(increment.Y) < 1)
+            {
+                smoothMoveTimer.Stop();
+            }
+        }
+
+        // when the trackbar value changes
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            // update the smoothness value based on the trackbar value
+            smoothness = trackBar1.Value;
+        }
+    }
+}
